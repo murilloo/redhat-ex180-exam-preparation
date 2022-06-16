68d7dc036448b724480f41879134a4e0dab4bbad
murillo <murillo@localhost.localdomain>
2022-06-16 16:27:22 +0200

    Update

--- simpleapp/index.php
+++ simpleapp/index.php
@@ -11,6 +11,6 @@
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
 		<p> <?php phpinfo(); ?> </p> 
-        <h2>BONJOUR GEZELLIGE GRIEZELS UIT APPLESCHA</h2>
+        <h2>HELLO LETS ROCK IT!</h2>
 	</body> 
 </html>
