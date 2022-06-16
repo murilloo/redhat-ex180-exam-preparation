69f26b2fcf70221a58ba2b6035b40032cbe25d3c
murillo <murillo@localhost.localdomain>
2022-06-16 16:39:36 +0200

    update

--- simpleapp/index.php
+++ simpleapp/index.php
@@ -9,8 +9,6 @@
 		<p><b>An Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information</h2> 
-		<p> <?php phpinfo(); ?> </p> 
         <h2>HELLO LETS ROCK IT!</h2>
 	</body> 
 </html>
